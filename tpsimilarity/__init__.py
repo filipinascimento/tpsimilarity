884eb8c81bb9ef668c1abac23f53d98b15352240
Filipi Nascimento Silva <filipinascimento@gmail.com>
2024-10-23 20:53:03 -0400

    Add `pyproject.toml` and remove `setup.py` to manage project metadata and dependencies
    * **`pyproject.toml`**
      - Add project metadata and dependencies
      - Set dynamic versioning using `__version__` from `tpsimilarity`
    
    * **`tpsimilarity/__init__.py`**
      - Add `__version__` attribute to dynamically fetch version from `pyproject.toml`
      - Use modern API for the path
    
    * **`.github/workflows/publish.yml`**
      - Add GitHub Actions workflow to publish to PyPI on version tag release
      - Use `pypa/gh-action-pypi-publish@release/v1` with no password

--- tpsimilarity/__init__.py
+++ tpsimilarity/__init__.py
@@ -1,2 +1,13 @@
 # tpdistance/__init__.py
 from tpsimilarity.similarity import *
+
+import toml
+from pathlib import Path
+
+def get_version():
+    pyproject_path = Path(__file__).parent.parent / 'pyproject.toml'
+    with open(pyproject_path, 'r') as f:
+        pyproject_data = toml.load(f)
+    return pyproject_data['project']['version']
+
+__version__ = get_version()
